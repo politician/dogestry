ef297a0904639ae0646ad627e74a4478260a190b
David Zarlengo <david@gratafy.com>
2016-04-20 11:28:53 -0700

    updates version string

--- src/dogestry/cli/version.go
+++ src/dogestry/cli/version.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-const Version string = "2.0.1"
+const Version string = "2.0.1 (https://github.com/politician/dogestry)"
 
 func PrintVersion() error {
 	_, err := fmt.Printf("Dogestry %s\n", Version)
