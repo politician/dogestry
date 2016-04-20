bcbbad2479cc659dce12ccf3260145aeee2a8508
David Zarlengo <david@gratafy.com>
2016-04-20 11:54:02 -0700

    improve backwards compatibility by stripping hash function from image ids

--- src/dogestry/remote/id.go
+++ src/dogestry/remote/id.go
@@ -1,8 +1,20 @@
 package remote
 
+import (
+	"strings"
+)
+
 type ID string
 
+// stripPrefix removes hashing definition that we are not interested in here.
+// Also makes sure as to not break backwards compatibility with older versions
+// where Docker did not specify the hash function.
+func (id ID) trimPrefix() ID {
+	return ID(strings.TrimPrefix(string(id), "sha256:"))
+}
+
 func (id ID) Short() ID {
+	id = id.trimPrefix()
 	shortLen := 12
 	if len(id) < shortLen {
 		shortLen = len(id)
@@ -11,5 +23,5 @@ func (id ID) Short() ID {
 }
 
 func (id ID) String() string {
-	return string(id)
+	return string(id.trimPrefix())
 }
